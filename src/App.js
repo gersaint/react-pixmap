59361bc6586a521c737e576f12f7a39942a6114e
Gersaint Cruz <gersa_int@outlook.com>
2019-12-09 14:19:17 -0600

    Props and States in react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import Buscador from "./componentes/Buscador.js";
 // import logo from './logo.svg';
 // import "./App.css";
 
-function App() {
-  return (
-    <div className="container">
-      <div className="jumbotron">
+class App extends Component {
 
-        <p className="lead text-center">Buscador de imagenes</p>
+  state = {
 
-        <Buscador />
-        
+    termino: 'cafe'
+    
+  }
+
+  datosBusqueda = ( termino ) => { 
+    
+    // console.log(termino);
+
+    this.setState({
+
+      termino
+      
+    })
+    
+  }
+
+  render() { 
+
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imagenes</p>
+          <Buscador
+            // mensaje="Buscador..." 
+            datosBusqueda = { this.datosBusqueda }
+          />
+        </div>
+        {this.state.termino}
       </div>
-    </div>
-  );
+    );
+
+
+  }
+  
 }
 
 export default App;
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,31 +1,48 @@
 import React, { Component } from "react";
 
 class Buscador extends Component {
-    state = {};
+  state = {};
 
-    render() {
-        return (
-        <form>
-            <div className="row">
-            <div className="form-group col-md-8">
-                <input
-                type="text"
-                className="form-control form-control-lg"
-                placeholder="Busca tu Imagen. Ejemplo: Skateboarding"
-                />
-            </div>
+  busquedaRef = React.createRef();
 
-            <div className="form-group col-md-4">
-                <input
-                type="submit"
-                className="btn btn-lg btn-danger btn-block"
-                value="Buscar.."
-                />
-            </div>
-            </div>
-        </form>
-        );
-    }
+  obtenerDatos = e => {
+    e.preventDefault(); // previene la accion por default del form
+
+    // console.log(this.busquedaRef.current.value);
+
+    // tomamos el valor del input
+    const termino = this.busquedaRef.current.value;
+
+    // lo enviamos al componente principal
+    this.props.datosBusqueda(termino);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.obtenerDatos}>
+        <div className="row">
+          <div className="form-group col-md-8">
+            <input
+              ref={this.busquedaRef}
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Busca tu Imagen. Ejemplo: Skateboarding"
+            />
+          </div>
+
+          <div className="form-group col-md-4">
+            <input
+              type="submit"
+              className="btn btn-lg btn-danger btn-block"
+              value="Buscar.."
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
+
+// document.querySelector('form').addEventListener('submit', function () {});
 
 export default Buscador;
