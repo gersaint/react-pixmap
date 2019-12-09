f38ee3667f0ffb0aef18648edf7d7b1bff3b3062
Gersaint Cruz <gersa_int@outlook.com>
2019-12-09 13:44:11 -0600

    Inicio Proyecto: import bootstrap, Nuevo componente: Buscador.js ( inputs: type: text and button/submit  )

---
+++ src/componentes/Buscador.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Buscador extends Component {
+    state = {};
+
+    render() {
+        return (
+        <form>
+            <div className="row">
+            <div className="form-group col-md-8">
+                <input
+                type="text"
+                className="form-control form-control-lg"
+                placeholder="Busca tu Imagen. Ejemplo: Skateboarding"
+                />
+            </div>
+
+            <div className="form-group col-md-4">
+                <input
+                type="submit"
+                className="btn btn-lg btn-danger btn-block"
+                value="Buscar.."
+                />
+            </div>
+            </div>
+        </form>
+        );
+    }
+}
+
+export default Buscador;
